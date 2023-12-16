2df0e21c5febb37fe0a9fcf1c338696eeb8ab121
markcao0915 <mark.cao915@gmail.com>
2023-12-16 03:56:21 +0000

    full code

--- obtain_google_search_results.py
+++ obtain_google_search_results.py
@@ -7,7 +7,8 @@
 # in a decreasing order in frequency of appearance.
 import time
 import pandas as pd
-import os
+import csv
+from openai import OpenAI
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -20,7 +21,10 @@ system_instruction = 'Translate the given English string into '
 driver_path = "chromedriver.exe"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+chrome_options.add_argument(
+    '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+client = OpenAI(api_key="openai_key")
+
 
 def getGoogleResults(name):
     # Set the path to the WebDriver executable
@@ -96,25 +100,43 @@ def translateMovieName(name, language):
     return chat.choices[0].message.content
 
 
+def write_dict_to_csv(data_dict, csv_file_path):
+    # Open the CSV file in write mode
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        # Extract keys and values separately
+        keys = list(data_dict.keys())
+        values = list(data_dict.values())
+
+        # Create a CSV writer
+        csv_writer = csv.writer(csv_file)
+
+        # Write the header to the CSV file
+        csv_writer.writerow(['domain', 'freq'])
+
+        # Write the data to the CSV file
+        csv_writer.writerows(zip(keys, values))
+
+
 def main():
     website_counts = {}
     movies = pd.read_csv("IMDB-Movie-Data.csv")
-    movie_names = list(movies["Title"][0: 20])
+    movie_names = list(movies["Title"][0: 10])
 
     for search_name in movie_names:
-        search_string = translateMovieName(search_name, 'Spanish')
-        search_results = getGoogleResults(search_string)
-        for result in search_results:
-            url = findIpAddress(result)
-            if url not in website_counts:
-                website_counts[url] = 0
-                # Increment the count for the element
-            website_counts[url] += 1
+        for language in ['English', 'Chinese', 'Spanish', 'Indi']:
+            search_string = translateMovieName(search_name, language)
+            search_results = getGoogleResults(search_string)
+            for result in search_results:
+                url = findIpAddress(result)
+                if url not in website_counts:
+                    website_counts[url] = 0
+                    # Increment the count for the element
+                website_counts[url] += 1
     # Sort the dictionary by values (in ascending order)
     sorted_dict = dict(sorted(website_counts.items(), key=lambda item: item[1], reverse=True))
 
     # Print the sorted dictionary by values
-    print(sorted_dict)
+    write_dict_to_csv(sorted_dict, "output.csv")
 
 
 if __name__ == "__main__":
